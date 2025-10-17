5dc6acc4b6b43b892f0b27e84f9ab036106c1438
Sebastian Canulef <95043068+SebaCanulef@users.noreply.github.com>
2025-10-17 16:19:20 -0300

    Fix indentation in home route return statement

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 ¡Hola desde Flask en Docker! 
-    prueba 2"
+    return "🚀 ¡Hola desde Flask en Docker! prueba 2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
