01d55be5a265b89aa5893b116c2ead1d33066abd
Sebastian Canulef <95043068+SebaCanulef@users.noreply.github.com>
2025-10-17 16:12:25 -0300

    Update home route message in main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 ¡Hola desde Flask en Docker! (probandoooo)"
+    return "🚀 ¡Hola desde Flask en Docker!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
